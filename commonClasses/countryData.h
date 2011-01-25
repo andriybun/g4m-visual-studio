a46e91d74eecbbb67c302ace016c48f00ee6ef8a
andr.bun <andr.bun@77ded694-db7c-03f5-14ee-59271a6f94bf>
2011-01-25 18:51:38 +0000

    Added countryData class

---
+++ commonClasses/countryData.h
@@ -0,0 +1,51 @@
+//  Name: CountryData class with statistics by regions
+//  Author: Andriy Bun
+//  Date: 29.10.09
+//  MG: added printToFile
+//  Description: 
+//      29.10.2009: added selecting countries to print; if no countries specified - prints all countries;
+
+
+#ifndef COUNTRYDATA_H_
+#define COUNTRYDATA_H_
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <set>
+#include "ipol.h"
+
+const int countriesNum = 242;
+
+using namespace std;
+
+class countryData
+{
+private:
+    typedef set<int> ST;
+    vector< g4m::ipol<double,double> > countries;
+    vector< g4m::ipol<double,int> > count;
+    vector< unsigned char > regions;
+//    ST countriesToPrint;
+    bool printAllCountries;
+    void setRegions();
+//    void PrintToFile(string, int, int, int, string);  // print array to file    
+public:
+    countryData();
+    ~countryData();
+    void reset(void);
+    void setV(int, int, double);
+    void inc(int, int, double);
+    double get(int, int);
+    double getAvg(int, int);
+    double getRegionSum(unsigned char regID, int year);
+    void insertCountryToPrint(int countryID);
+    ST getListOfCountries();
+    ST countriesToPrint;    
+    void setListOfCountries(ST S);
+    void PrintToFile(string, int, int, int, string);  // print array to file    
+};
+
+#endif
