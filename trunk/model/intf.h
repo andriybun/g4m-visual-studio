4ac6243cd56b4f68e2cfc83e0e87338f83a2c61d
andr.bun <andr.bun@77ded694-db7c-03f5-14ee-59271a6f94bf>
2011-03-09 17:31:39 +0000

    Added threading module to g4m

---
+++ trunk/model/intf.h
@@ -0,0 +1,21 @@
+#ifndef INTF_H_
+#define INTF_H_
+
+#include "threading.h"
+
+#define MAX_ARRAY_SIZE		20
+
+typedef struct inDataStruct
+{
+	
+} 
+inDataStructT;
+
+typedef struct outDataStruct
+{
+	
+} 
+outDataStructT;
+
+
+#endif
