7acb6041749bc7a4f947b1676c2b06a774e14fd0
andr.bun <andr.bun@77ded694-db7c-03f5-14ee-59271a6f94bf>
2011-03-10 14:04:23 +0000

    Added min and max macroses to model.h

--- model/model.h
+++ model/model.h
@@ -1,7 +1,12 @@
 #ifndef MODEL_H_
 #define MODEL_H_
 
+#include <cmath>
+
 #include "intf.h"
+
+#define min(x, y) ((x)<(y) ? (x):(y))
+#define max(x, y) ((x)<(y) ? (y):(x))
 
 template <class inDataT, class outDataT>
 int modelFunc(inDataT &inData, outDataT &outData)
