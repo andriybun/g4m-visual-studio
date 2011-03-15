7a87888f807592a9b33cba7a0cb6ef9a3eca2336
andr.bun <andr.bun@77ded694-db7c-03f5-14ee-59271a6f94bf>
2011-03-15 16:23:00 +0000

    Updated build_with_shared.cmd script to build entire solution

--- build_with_shared.cmd
+++ build_with_shared.cmd
@@ -6,11 +6,17 @@ set OUT_BINARY_DIR=Bin_gcc
 set SOLUTION_NAME=g4m
 
 call %~dp0\_build_shared.cmd %GCC_PATH% commonClasses %OUT_BINARY_DIR% || exit /b 1
-call %~dp0\_build_shared.cmd %GCC_PATH% jDataViewClasses %OUT_BINARY_DIR% || exit /b 1
+call %~dp0\_build_shared.cmd %GCC_PATH% jDataViewClasses %OUT_BINARY_DIR% || exit /b 1rem 
 
-rem %GCC_PATH% %STARTUP_PROJECT_NAME%\*.cpp %OUT_BINARY_DIR%\*.lib -I .\commonClasses -I .\jDataViewClasses -O %OUT_BINARY_DIR%\%SOLUTION_NAME% -O2
+pushd %~dp0\%STARTUP_PROJECT_NAME%
+
+call %GCC_PATH% -c *.cpp -I ..\commonClasses -I ..\jDataViewClasses -O2 || popd exit /b 1
+call %GCC_PATH% *.o ..\%OUT_BINARY_DIR%\*.lib -o "..\%OUT_BINARY_DIR%\%SOLUTION_NAME%" || exit /b 1
+
+del *.o
+
+popd
 
 echo on
 
 exit /b 0
-
