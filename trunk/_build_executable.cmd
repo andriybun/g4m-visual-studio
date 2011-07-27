e28bbfba52615825f68321e34e704ef742623fd9
andr.bun <andr.bun@77ded694-db7c-03f5-14ee-59271a6f94bf>
2011-07-27 15:42:22 +0000

    started building with gcc

---
+++ trunk/_build_executable.cmd
@@ -0,0 +1,18 @@
+set GCC_PATH=%1
+set PROJECT_NAME=%2
+set BIN_PATH=..\%3
+
+pushd %~dp0\%PROJECT_NAME%
+
+if not exist %BIN_PATH% (
+    md %BIN_PATH%
+)
+
+%GCC_PATH% -fPIC -c *.cpp || exit /b 1
+%GCC_PATH% -o %BIN_PATH%\%PROJECT_NAME%.exe *.o || exit /b 2
+
+del *.o
+
+popd
+
+
--- trunk/build_with_shared.cmd
+++ trunk/build_with_shared.cmd
@@ -6,7 +6,9 @@ set OUT_BINARY_DIR=Bin_gcc
 set SOLUTION_NAME=g4m
 
 call %~dp0\_build_shared.cmd %GCC_PATH% commonClasses %OUT_BINARY_DIR% || exit /b 1
-call %~dp0\_build_shared.cmd %GCC_PATH% jDataViewClasses %OUT_BINARY_DIR% || exit /b 1rem 
+call %~dp0\_build_shared.cmd %GCC_PATH% jDataViewClasses %OUT_BINARY_DIR% || exit /b 1
+
+call %~dp0\_build_executable.cmd %GCC_PATH% metadata_generator %OUT_BINARY_DIR% || exit /b 1 
 
 pushd %~dp0\%STARTUP_PROJECT_NAME%
 
